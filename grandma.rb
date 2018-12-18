18aa05054e1d5ef1dcbf3768a924e34e010c6c12
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-18 17:26:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+  if phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
